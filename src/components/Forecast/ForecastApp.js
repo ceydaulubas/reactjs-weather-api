19f50af0e4632a434ecd25047d4fba613c197c12
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2021-04-09 20:08:10 +0300

    created react and axios and added searchbar and

---
+++ src/components/Forecast/ForecastApp.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+import SearchBar from '../Search/SearchBar'
+
+const ForecastApp = () => {
+    const [searchBarInput, setSearchBarInput] = useState("")
+    const [forecastListResults, setForecastListResults] = useState([]);
+
+    const handleSearchForForecast = () => {
+
+        const API_KEY = "f13c4a1e0110a21f84487056745a3c76";
+        const searchInput = searchBarInput ? searchBarInput : "istanbul";
+
+        axios
+            .get(`http://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${API_KEY}`)
+            .then((forecastResults) => setForecastListResults(forecastResults.data))
+            .catch((error) => console.error(`Someting went wrong: ${error}`));
+    };
+
+    // useEffect() is used to mimic the behavior of componentDidMount() & componentDidUpdate()
+    // useEffects takes two parameters:
+    //  1. handleSearchForForecast - A callback function which is the action that should happen when a component either mounts or is updated.
+    // 2. [searchBarInput] - The value that is monitored when an update occurs.
+    useEffect(handleSearchForForecast, [searchBarInput]);
+
+    // Function handler for updating the 'searchInput' state.
+    // This function is passed down as a prop and used in the SearchBar component.
+    const handleAddSearchBarInputToAppState = (searchInput) =>
+        setSearchBarInput(searchInput);
+
+
+    return (
+        <div>
+            <SearchBar addSearchInput={handleAddSearchBarInputToAppState}/>
+        </div>
+    )
+}
+
+export default ForecastApp;
---
+++ src/components/Search/SearchBar.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const initialState = "";
+
+const SearchBar = ({ addSearchInput }) => {
+
+    const [searchState, setSearchState] = useState(initialState);
+
+    // Function handler that checks for the changes in the input fields
+    const handleInputChange = (event) => {
+        setSearchState(event.target.value);
+    };
+
+    // Function handler that handles submitting the form
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        // Call a function that will allow for the form to lift it's state to the main application.
+        addSearchInput(searchState);
+
+         // reset the state of the form
+        setSearchState(initialState);
+    };
+
+    return (
+        <from onSubmit = {handleFormSubmit}>
+            <input
+                type="text"
+                name="search"
+                placeholder="Search City for forecast.."
+                value={searchState}
+                onChange={handleInputChange}
+            />
+            <button type="submit">Search</button>
+        </from>
+    );
+}
+
+export default SearchBar;
